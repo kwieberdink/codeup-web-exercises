7ab3d6cc4dc20d66f8c7b733be0163b7a3fbafa4
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-02-13 14:52:03 -0600

    showcasing conditionals

---
+++ js/conditionals-lesson.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// let flavor = prompt(`Welcome to Codeup Ice Cream!
+// What flavor do you want?`);
+
+// if (flavor === "chocolate") {
+//     alert(`One chocolate coming right up!`);
+// } else if(flavor==="vanilla") {
+//     alert(`One vanilla coming right up!`)
+// } else {
+//     alert(`We don't have that flavor, sorry :(`)
+// }
+
+// switch(flavor.toLowerCase()) {
+//     case"chocolate":// Parenthesis are not required, but they do work;
+//         alert("One chocolate coming right up!");
+//         break;
+//     case "vanilla":
+//         alert("One vanilla coming right up!");
+//         break;
+//     case "strawberry":
+//         alert("One strawberry coming right up!")
+//         break;
+//     default:
+//         alert("We don't have that flavor, sorry");
+// }
+//Without the break statement, not only will it give you the alert
+//for the one you want, but it will give you every alert
+
+// function serveIceCream(flavor) {
+//     if (flavor === "chocolate"
+//     || flavor === "vanillas"
+//     || flavor === "strawberry"){
+//         alert(`One ${flavor} coming right up!`);
+//     }else{
+//         alert("We don't have that flavor, sorry!");
+//     }
+// }
+// serveIceCream(prompt(`Welcome to Codeup Ice Cream!
+// What flavor do you want?`).toLowerCase());
+
+// function divisibleByFive(number) {
+//     if(number % 5 === 0){
+//         console.log("Divisible by 5!");
+//     } else {
+//         console.log("Not divisible by 5!");
+//     }
+// }
+
+// const divisibleByFiveTernary = number => {
+//     number % 5 === 0 ?  console.log("Divisible by 5!"):
+//         console.log("Not divisible by 5!");
+// } // When you have only one parameter, you dont need parenthesis
+//
+// const divisible = (dividend, divisor) => {
+//     dividend % divisor === 0 ? console.log(`Divisible by ${divisor}!`):
+//         console.log(`Not divisible by ${divisor}`);
+// }
+
+// const divisible = (dividend, divisor) => {
+//     return dividend % divisor === 0 ? `Divisible by ${divisor}!`
+//         : `Not divisible by ${divisor}!`;
+// }
+
+function divisible (dividend,divisor) {
+    if(dividend % divisor === 0) {
+        return true;
+    } else {
+        return false;
+    }
---
+++ js/functions-practice.js
@@ -0,0 +1,147 @@
+// Function Practice:
+
+
+//1. Make a function called returnTwo() that returns the number 2 when called
+//    Test this function with `console.log(returnTwo())`
+
+// const returnTwo= ()=> {
+//     let message = 2;
+//     return message;
+// }
+// console.log(returnTwo());
+
+// 2. Make a function called sayHowdy() which console.logs the string “Howdy!”
+//    Test this function by directly calling `sayHowdy()`
+//    Remember this function does not need a defined return value
+
+// const sayHowdy = (string) => {
+//     let message = `Howdy!`;
+//     return message;
+// }
+// console.log(sayHowdy());
+
+// 3. Make a function called returnName() that returns the string of your name
+//    Test this function with `console.log(returnName())`
+
+// const returnName = (name) => {
+//     let message = `${name}`
+//     return message
+// }
+// console.log(returnName(`Kaeden`));
+
+// 4. Make a function called addThree() which takes in a number input and returns the number plus 3.
+//    Test this function with `console.log(addThree(5))`
+
+// const addThree = (number) => {
+//     let message = number + 3;
+//     return message;
+// }
+// console.log(addThree(5));
+
+// 5. Make a function called sayString() which returns the string input passed in.
+//    Test this function with `console.log(sayString('codeup'))`
+
+// const sayString = (string) => {
+//     let message = string
+//     return string
+// }
+// console.log(sayString(`codeup`))
+
+// - Write a function named `lowerCase(string)`
+
+// const lowerCase = (string) => {
+//     let message = (string.toLowerCase())
+//     return message
+// }
+// console.log(lowerCase(`CODEUP`))
+
+// - Write a function named `double(n)` that returns a number times two
+
+// const double = (number) => {
+//     let message = number * 2;
+//     return message;
+// }
+// console.log(double(22))
+
+// - Write a function named `triple(n)` that returns a number times 3
+
+// const triple = (number) => {
+//     let message = number * 3;
+//     return message;
+// }
+// console.log(triple(6));
+
+// - Write a function named `quadruple(n)` that returns a number times 4
+
+// const quadruple =(number)=> {
+//     let message = number * 4;
+//     return message;
+// }
+// console.log(quadruple(4));
+
+// - Write a function named `half(n)` that returns 1/2 of the provided input
+
+// const half=(number)=> {
+//     let message = number / 2
+//     return message
+// }
+// console.log(half(3))
+
+// - Write a function named `subtract(a, b)` that returns `a` minus `b`
+
+// const subtract = (a, b) => {
+//     let message = a - b;
+//     return message;
+// }
+// console.log(subtract(4,2));
+
+// - Write a function named `multiply(a, b)` that returns the product of `a` times
+//   `b`
+
+// const multiply = (a,b)=> {
+//     let message = a * b;
+//     return message;
+// }
+// console.log(multiply(3,6));
+
+// - Write a function named divide(a, b) that returns `a` divided by ~b~
+
+// const divide = (a,b) => {
+//     let message = a / b;
+//     return message;
+// }
+// console.log(divide(12,3));
+
+// - Write a function named `remainder(a, b)` that returns the remainder after
+//   dividing a by b
+
+// const remainder = (a,b) => {
+//     let message = a % b;
+//     return message;
+// }
+// console.log(remainder(5,2));
+
+// - Write a function named `cube(n)` that returns n * n * n
+
+// const cube = (number) => {
+//     let message = number * number * number;
+//     return message;
+// }
+// console.log(cube(3));
+
+// - Write a function named `squareRoot(n)` that returns the square root of the
+//   input
+
+// const squareRoot = (number) => {
+//     let message = Math.sqrt(number);
+//     return message;
+// }
+// console.log(squareRoot(121));
+
+// - Write a function named `cubeRoot(n)` that returns the cube root of the input
+
+// const cubeRoot = (number) => {
+//     let message = Math.cbrt(number);
+//     return message;
+// }
+// console.log(cubeRoot(27));
